16e304f452033acb6496a2a12be80cae60cab250
Jared Fleming <jaredmf10@gmail.com>
2020-01-12 00:28:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,24 @@
 # Write your code here.
-# deli is empty
-def katz_deli
-  []
+def katz_deli []
 end
+
+ticket_number = 1
+
 # create take_a_number method accepting two arguments, the array for the current line of people (`katz_deli`), and a string containing the name of the person joining the end of the line
 def take_a_number(katz_deli, name)
   # add person to end of line
   katz_deli << name
-  # call out (`puts`) the person's name along with their position in line
+  # call out (`puts`) the person's name along with their position in line using hash
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
-# create a line
+
+# def take_a_number(katz_deli)
+# ticket_number += 1
+#   katz_deli << ticket_number
+#   puts "Welcome you have ticket number: #{ticket_number}"
+# end
+#
+  # create a line
 def line(katz_deli)
   # if the line is empty, say the line is empty
     if katz_deli.length == 0
@@ -21,6 +29,7 @@ def line(katz_deli)
       katz_deli.each.with_index(1) do |person, index|
       current_line << " #{index}. #{person}"
     end
+    # last line of method gets returned
     puts current_line
     end
 end
